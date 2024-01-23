7b0be7b85218a8b4fe60a16a36a95b2dd9ae809f
dtscott9 <dtscott9@byui.edu>
2024-01-23 12:43:30 -0700

    Fixed bug, added db name in contact controller

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -2,7 +2,7 @@ const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAllContacts = async (req, res, next) => {
-  const result = await mongodb.getDb().db().collection('contacts').find();
+  const result = await mongodb.getDb().db('cse341').collection('contacts').find();
   result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists); 
@@ -11,7 +11,7 @@ const getAllContacts = async (req, res, next) => {
 
 const getOneContact = async (req, res, next) => {
     const contactId = new ObjectId(req.params.id)
-    const result = await mongodb.getDb().db().collection('contacts').find({_id: contactId});
+    const result = await mongodb.getDb().db('cse341').collection('contacts').find({_id: contactId});
     result.toArray().then((lists) => {
       res.setHeader('Content-Type', 'application/json');
       res.status(200).json(lists[0]); 
